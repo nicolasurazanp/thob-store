889f50c7cc7d065f9242019c60a1bd6c3bfc3bcb
Nicolasurazanp <nicolasurazanp@gmail.com>
2025-09-10 18:49:20 -0500

    Segundo ajuste vista celular

--- src/components/AnnouncementBar.js
+++ src/components/AnnouncementBar.js
@@ -1,13 +1,13 @@
 
 import React from 'react';
-import { FaInstagram } from 'react-icons/fa'; 
+// import { FaInstagram } from 'react-icons/fa'; 
 import './AnnouncementBar.css'; 
 
 const AnnouncementBar = () => {
     return (
         <div className="announcement-bar">
             <div className="announcement-content">
-                <FaInstagram className="instagram-icon" />
+                {/* <FaInstagram className="instagram-icon" /> */}
                 <span>Envíos GRATIS por compras superiores a $180.000 &nbsp;|&nbsp; Compra mínima 70.000</span>
             </div>
         </div>
